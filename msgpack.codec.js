5201e722122cc57635380aa30f743710599dce8d
César D. Rodas <crodas@php.net>
2012-09-26 19:14:35 -0400

    Fixed bug with hashes with large keys
    If a key is largest than 54 bytes it was failing.

--- msgpack.codec.js
+++ msgpack.codec.js
@@ -343,18 +343,7 @@ function decode() { // @return Mix:
     case 0xde:  num += (buf[++_idx] << 8)       +  buf[++_idx];
     case 0x80:  hash = {};
                 while (num--) {
-                    // make key/value pair
-                    size = buf[++_idx] - 0xa0;
-
-                    for (ary = [], i = _idx, iz = i + size; i < iz; ) {
-                        c = buf[++i]; // lead byte
-                        ary.push(c < 0x80 ? c : // ASCII(0x00 ~ 0x7f)
-                                 c < 0xe0 ? ((c & 0x1f) <<  6 | (buf[++i] & 0x3f)) :
-                                            ((c & 0x0f) << 12 | (buf[++i] & 0x3f) << 6
-                                                              | (buf[++i] & 0x3f)));
-                    }
-                    _idx = i;
-                    hash[_toString.apply(null, ary)] = decode();
+                    hash[decode()] = decode();
                 }
                 return hash;
     // 0xdd: array32, 0xdc: array16, 0x90: array
